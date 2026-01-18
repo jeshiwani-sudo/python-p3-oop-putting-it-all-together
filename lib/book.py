74deec2d944460b56eb3d3a7efbb7b34b30c908c
James Eshiwani <james.eshiwani@student.moringaschool.com>
2026-01-18 11:41:35 +0300

    put it together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self._title = None
+        self._page_count = None
         
+        self.title = title
+        self.page_count = page_count
+
+    def title(self):
+        return self._title
+
+    def title(self, value):
+        self._title = value
+
+    def page_count(self):
+        return self._page_count
+
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self._brand = None
+        self._size = None  
+
+        self.brand = brand
+        self.size = size
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        self._brand = value
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+            if not hasattr(self, '_size'):
+                self._size = None
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
